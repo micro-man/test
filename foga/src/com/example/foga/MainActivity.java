bd2041f07a08c3f1d4eef9b0f06195bbaf91ea35
micro-man <kenshi2580>
2013-07-29 16:48:23 +0900

    main move

--- foga/src/com/example/foga/MainActivity.java
+++ foga/src/com/example/foga/MainActivity.java
@@ -3,17 +3,43 @@ package com.example.foga;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
 
+	TextView tv1;
+	Button btn1;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        findview();
+        
+        btn1.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				tv1.setText("hello");
+				
+			}
+		});
     }
 
 
-    @Override
+    private void findview() {
+		
+		tv1 = (TextView)findViewById(R.id.tv1);
+		btn1 = (Button)findViewById(R.id.btn1);
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
